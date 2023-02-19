e1b0bac445075a861121274d8c81a2d2b5e4b82c
Mayla Rocha <maylaetb@gmail.com>
2023-02-19 01:17:09 -0300

    criado modulo animais

---
+++ gatitobook/src/app/animais/lista-de-animais/lista-de-animais.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaDeAnimaisComponent } from './lista-de-animais.component';
+
+describe('ListaDeAnimaisComponent', () => {
+  let component: ListaDeAnimaisComponent;
+  let fixture: ComponentFixture<ListaDeAnimaisComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaDeAnimaisComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListaDeAnimaisComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
