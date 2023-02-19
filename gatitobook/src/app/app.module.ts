539bc272aabd44dd2e7fb7e48bc811e1b53125fe
Mayla Rocha <maylaetb@gmail.com>
2023-02-19 01:05:27 -0300

    importando o HttpClientModule

--- gatitobook/src/app/app.module.ts
+++ gatitobook/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,7 +13,8 @@ declarations: [
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ]
 })
 export class AppModule { }
