84fead080c592f1b38c9dd32f2841f613a51b6de
Mayla Rocha <maylaetb@gmail.com>
2023-03-01 17:36:14 -0300

    criação método verificaUsuarioExistente

--- gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -10,7 +10,10 @@ export class NovoUsuarioService {
   constructor(private http: HttpClient) { }
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.http.post('http://localhost:3000/use4r/signup', novoUsuario);
+    return this.http.post('http://localhost:3000/user/signup', novoUsuario);
   }
 
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.http.get('http://localhost:3000/user/exists/${ nomeUsuario }')
+  }
 }
